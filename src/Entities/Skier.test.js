f5dc986023b39bbfa7218f95d9d0a84ab5fb0cb3
anneliesez <anneliese505@gmail.com>
2020-08-15 15:00:07 -0600

    100% code coverage on Skier turnLeft and turnRight functions

--- src/Entities/Skier.test.js
+++ src/Entities/Skier.test.js
@@ -1,9 +1,138 @@
 import * as Constants from "../Constants";
 import { Skier } from "./Skier";
 
-test('On turnLeft after crash, skier faces left',() => {
-    let skier = new Skier(0, 0);
-    skier.setDirection(Constants.SKIER_DIRECTIONS.CRASH);
-    skier.turnLeft();
-    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT);
+describe('Skier', () => {
+
+    let skier;
+
+    beforeEach(() => {
+        skier = new Skier(0,0);
+    });
+
+    describe('that has CRASHed', () => {
+
+        beforeEach(() => {
+            skier.setDirection(Constants.SKIER_DIRECTIONS.CRASH);
+        });
+
+        test('should face LEFT on turnLeft',() => {
+            skier.turnLeft();
+            expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT);
+        });
+
+        test('should face RIGHT on turnRight',() => {
+            skier.turnRight();
+            expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT);
+        });
+
+    });
+
+    describe('that is facing LEFT', () => {
+
+        beforeEach(() => {
+            skier.setDirection(Constants.SKIER_DIRECTIONS.LEFT);
+        });
+
+        test('should face LEFT on turnLeft',() => {
+            skier.turnLeft();
+            expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT);
+        });
+
+        test('should move LEFT on turnLeft',() => {
+            const moveLeftSpy = jest.spyOn(skier,'moveSkierLeft');
+            skier.turnLeft();
+            expect(moveLeftSpy).toHaveBeenCalled();
+        });
+
+        test('should face LEFT_DOWN on turnRight',() => {
+            skier.turnRight();
+            expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT_DOWN);
+        });
+
+    });
+
+    describe('that is facing LEFT_DOWN', () => {
+
+        beforeEach(() => {
+            skier.setDirection(Constants.SKIER_DIRECTIONS.LEFT_DOWN);
+        });
+
+        test('should face LEFT on turnLeft',() => {
+            skier.turnLeft();
+            expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT);
+        });
+
+        test('should face DOWN on turnRight',() => {
+            skier.turnRight();
+            expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.DOWN);
+        });
+
+    });
+
+    describe('that is facing DOWN', () => {
+
+        beforeEach(() => {
+            skier.setDirection(Constants.SKIER_DIRECTIONS.DOWN);
+        });
+
+        test('should face LEFT_DOWN on turnLeft',() => {
+            skier.turnLeft();
+            expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT_DOWN);
+        });
+
+        test('should face RIGHT_DOWN on turnRight',() => {
+            skier.turnRight();
+            expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT_DOWN);
+        });
+
+    });
+
+    describe('that is facing RIGHT', () => {
+
+        beforeEach(() => {
+            skier.setDirection(Constants.SKIER_DIRECTIONS.RIGHT);
+        });
+
+        test('should face RIGHT on turnRight',() => {
+            skier.turnRight();
+            expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT);
+        });
+
+        test('should move RIGHT on turnRight',() => {
+            const moveRightSpy = jest.spyOn(skier,'moveSkierRight');
+            skier.turnRight();
+            expect(moveRightSpy).toHaveBeenCalled();
+        });
+
+        test('should face RIGHT_DOWN on turnLeft',() => {
+            skier.turnLeft();
+            expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT_DOWN);
+        });
+
+    });
+
+    describe('that is facing RIGHT_DOWN', () => {
+
+        beforeEach(() => {
+            skier.setDirection(Constants.SKIER_DIRECTIONS.RIGHT_DOWN);
+        });
+
+        test('should face RIGHT on turnRight',() => {
+            skier.turnRight();
+            expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT);
+        });
+
+        test('should face DOWN on turnLeft',() => {
+            skier.turnLeft();
+            expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.DOWN);
+        });
+
+    });
+
+
+
+
+
 });
+
+
